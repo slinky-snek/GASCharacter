a245dcf5e7fef33242d21746a4249f7822de7cd9
Aaron Barnett <64507952+slinky-snek@users.noreply.github.com>
2023-07-27 11:00:03 -0400

    Update HeroCharacter.cpp

--- Private/HeroCharacter.cpp
+++ Private/HeroCharacter.cpp
@@ -2,10 +2,8 @@
 #include "BoppersAttributeSet.h"
 #include "BoppersAbilitySystemComponent.h"
 
-// Sets default values
 AHeroCharacter::AHeroCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	AbilitySystemComponent = CreateDefaultSubobject<UBoppersAbilitySystemComponent>("AbilitySystemComponent");
@@ -13,7 +11,6 @@ AHeroCharacter::AHeroCharacter()
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 }
 
-// Called when the game starts or when spawned
 void AHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
@@ -36,7 +33,6 @@ void AHeroCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
-// Called to bind functionality to input
 void AHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
